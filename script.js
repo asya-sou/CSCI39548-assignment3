c96990efcf3c9b06ac96cea7e62375a79d16b499
asya-sou <asyasoughou@gmail.com>
2021-06-14 07:02:48 -0400

    some notes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//cells are elements
+//create cell rows 
+//document.getElementById() === $()
+//<td> table data cell element
+//tr - row in a table
+//col - column 
+// @ @ @ @
+// @ @ @ @ 
+// ^ to add col - for each row add 1 elment to end of row
+// ^ to add row - for each col add 1 element to bottom of the col
+// createElement() cells
+// use appendChild() to attach
+// Node.childNodes - will contain all child elements &non-elements of an el on which called
+// Node.lastElementChild - return last child element of an element
 
